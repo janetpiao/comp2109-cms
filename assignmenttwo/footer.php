9d3ccf7cabfbfefa46cf7ac8936806f984ce585e
janetpiao <janetpiao66@gmail.com>
2025-04-02 21:46:45 -0400

    Finished Assignment2

---
+++ assignmenttwo/footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <section class="top-footer">
+        <div class="first-widget">
+            <?php dynamic_sidebar('footer-widget-area-one'); ?>
+        </div>
+        <div class="second-widget">
+            <?php dynamic_sidebar('footer-widget-area-two'); ?>
+        </div>
+
+        <div class="third-widget">
+            <?php dynamic_sidebar('footer-widget-area-three'); ?>
+        </div>
+        <div class="fourth-widget">
+            <?php dynamic_sidebar('footer-widget-area-four'); ?>
+        </div>
+    </section>
+</footer>
+</body>
+
+</html>
---
+++ assignmenttwo/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="<?php language_attributes(); ?>">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <!-- add our CSS -->
+    <link rel="stylesheet" href="<?php echo esc_url(home_url('wp-content/themes/assignmenttwo/style.css')); ?>">
+</head>
+
+
+<body <?php body_class(); ?>>
+    <header class="default-header">
+        <div class="logo">
+            <a href="<?php echo esc_url(home_url()); ?>">
+                <img src="<?php echo esc_url(home_url('wp-content/uploads/2025/02/logo-reader.png'))?>"
+                    alt="header logo"></a>
+        </div>
+        <nav>
+            <?php 
+                wp_nav_menu(array(
+                    'menu'     => 'main',
+                    'theme_location' => '',
+                    'depth'    => 2,
+                    'fallback_cb' => false
+                ));
+            ?>
+        </nav>
+    </header>
