af3fcc870dafec02c848405bdbf6a25cd72fd390
janetpiao <janetpiao66@gmail.com>
2025-04-07 20:37:15 -0400

    Show product on home page using template

--- majorproject/page-templates/homepage-template.php
+++ majorproject/page-templates/homepage-template.php
@@ -14,18 +14,10 @@ get_header();
     </section>
     <section class="home-row-one">
         <h3><?php echo wp_kses_post(get_field('row_one_title')); ?></h3>
-        <img src="<?php echo wp_kses_post(get_field('row_one_image')); ?>"
-            alt="<?php echo wp_kses_post(get_field('row_one_image_alt')); ?>">
         <p><?php echo wp_kses_post(get_field('row_one_text')); ?></p>
     </section>
-    <section class="home-row-two">
-        <h3><?php echo wp_kses_post(get_field('row_two_title')); ?></h3>
-        <p><?php echo wp_kses_post(get_field('row_two_text')); ?></p>
-        <label for="row_two_email"><?php echo esc_html(get_field('row_two_email_label')); ?></label>
-        <input type="email" name="row_two_email" id="row_two_email">
-    </section>
     <section class="shortcode">
-        <?php  echo do_shortcode("[cmsPost]");?>
+        <?php  echo do_shortcode("[products limit=4 columns=4 order=desc]");?>
     </section>
 </main>
 
