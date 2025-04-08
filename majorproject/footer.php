4588bd88b689bf97cc5f34d1da2ca5f3c1084d19
janetpiao <janetpiao66@gmail.com>
2025-04-07 21:07:45 -0400

    Styled homepage template and post template. Modified footer.

--- majorproject/footer.php
+++ majorproject/footer.php
@@ -6,13 +6,6 @@
         <div class="second-widget">
             <?php dynamic_sidebar('footer-widget-area-two'); ?>
         </div>
-
-        <div class="third-widget">
-            <?php dynamic_sidebar('footer-widget-area-three'); ?>
-        </div>
-        <div class="fourth-widget">
-            <?php dynamic_sidebar('footer-widget-area-four'); ?>
-        </div>
     </section>
 </footer>
 </body>
